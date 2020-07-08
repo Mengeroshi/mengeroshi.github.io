10f15dadad696e23301531b850f0d35316cbd378
mengeroshi <dpllgr@hotmail.com>
2020-07-08 17:00:58 -0500

    swipper gallery about me implemented

---
+++ swiperGallery.js
@@ -0,0 +1,10 @@
+new Swiper('#about_me__gallery',{
+    speed: 400,
+    spaceBetween: 100,
+    navigation:{
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+    effect: "flip",
+    loop: true,
+});  
