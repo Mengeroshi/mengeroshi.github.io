2d91de044123925d3cc77916eda314a87ae9f5a2
mengeroshi <dpllgr@hotmail.com>
2020-07-13 13:22:16 -0500

    lectures carrousell modeled

--- swiperGallery.js
+++ swiperGallery.js
@@ -5,6 +5,24 @@ new Swiper('#about_me__gallery',{
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev"
     },
-    effect: "flip",
+    effect: "cube",
     loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+});
+
+new Swiper('#lectures__gallery',{
+    slidesPerView: 1,
+    spaceBetween: 300,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
 });  
