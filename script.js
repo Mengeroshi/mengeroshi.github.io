31db0accffbabdd7f68df5d4d38942b28baa0124
mengeroshi <dpllgr@hotmail.com>
2020-07-22 15:38:38 -0500

    garbage removed

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const navBar = document.querySelectorAll('.nav_bar__item');
 const sections = document.querySelectorAll('section');
 const powarade = '#1DD0F8';
 const blueDemon = '#043F63';
-let obo = sections[0].getBoundingClientRect().height + sections[1].getBoundingClientRect().height +sections[2].getBoundingClientRect().height +sections[3].getBoundingClientRect().height
+
 
 
 function defaultColors(){
@@ -28,8 +28,6 @@ window.addEventListener('scroll', ()=>{
             sections[3],
             sections[4]
           );
-        console.log('scrolled',scrolled);
-        console.log('distance', distance);
 
         if(scrolled >= sections[4].offsetTop - (distance/1.2)){
             defaultColors()
@@ -52,11 +50,7 @@ window.addEventListener('scroll', ()=>{
                     defaultColors()
                     highlighted(navBar[4]);
                     break;
-                /*case  'contact':
-                    defaultColors()
-                    highlighted(navBar[5]);
-                    break;
-                */
+                
                
                 
             }
